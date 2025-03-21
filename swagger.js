cfd68ede45cf26d7b3d29a161765e6ce7a4c83dc
Marcel Nyembo <marcelmusuyu9@gmail.com>
2025-03-21 15:05:46 +0000

    Updating documentation online url

--- swagger.js
+++ swagger.js
@@ -9,7 +9,7 @@ const doc = {
     title: 'REST API for CSE341', 
     description: ' This is a REST API for CSE341 course. The purpose of this API is to provide a way to manage contacts. This API will allow you to create, read, update, and delete contacts. This API is built using Node.js, Express, and MongoDB. The API is hosted on Render and the database is hosted on MongoDB Atlas. The API is secured using JWT tokens. The API is documented using Swagger. The API is tested using Postman. The API is deployed using GitHub Actions', 
   },
-  host: 'https://cse341-spring25.onrender.com/api-docs/'|| 'localhost:3000', // Change to your host and port
+  host: 'https://cse341-spring25.onrender.com'|| 'localhost:3000', // Change to your host and port
   schemes: ['https'] || ['http'], // Change to https if you're using https
 };
 
